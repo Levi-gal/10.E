dc86416224657ad7f511debcb66311cd481f5172
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2021-10-04 10:07:27 +0200

    Add files via upload

---
+++ ora.py
@@ -0,0 +1,44 @@
+user_name = input('Add meg a felhasználó nevedet: ')
+print('Legyen szép napja kedves' ,user_name,)
+
+
+print('Szia ' + input('Neved? ' ) + '!')
+
+
+print('**********')
+print('oooooooooo')
+
+
+print('*o*o*o*o*o')
+print('*o*o*o*o*o')
+
+
+first_number = int(input('Enter the first number '))
+second_number = int(input('Enter the second number '))
+
+print( first_number + second_number )
+
+
+first_number = int(input('Enter the first number '))
+second_number = int(input('Enter the second number '))
+third_number = int(input('Enter the thrid number '))
+
+print( first_number + second_number + third_number )
+
+
+first_number = int(input('Enter the first number '))
+second_number = int(input('Enter the second number '))
+
+print( first_number - second_number )
+
+
+first_number = int(input('Enter the first number '))
+second_number = int(input('Enter the second number '))
+
+print( first_number * second_number )
+
+
+first_number = int(input('Enter the first number '))
+second_number = int(input('Enter the second number '))
+
+print( first_number / second_number )
