9ed2fdab1ae44e919362a5c0958e9c664ca63021
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2021-10-05 14:42:58 +0200

    Add files via upload

---
+++ hazifeladat.py
@@ -0,0 +1,10 @@
+cm = int(input('Magaság: '))
+
+if (170 > cm):
+    print('170 alatt te törpe vagy')
+
+elif(180 < cm):
+    print('180 fölött te zsiráf vagy')
+
+else:
+    print('Te közepesen átlagos vagy')
